a17f4dd4092576dd43f786db246e58c1ecda9c92
leoniliolrs <leoniliolrs@gmail.com>
2019-09-18 16:01:43 -0300

    aplicação em django pt-br

---
+++ calculadoraTRL/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/cti/Dropbox/ProjetosPython/calculadoraTRL/calculadoraTRL/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
